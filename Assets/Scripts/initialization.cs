25d07fab9cac5d79148ff24ed90045a19d825bc5
CoreyHickson <corey.r.hickson@gmail.com>
2015-09-26 00:05:03 -0600

    Uploading my Unit GameJame2015 project, initial files

---
+++ Assets/Scripts/initialization.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class initialization : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(waitRandom());
+    }
+
+    // A callable coroutine which waits between 3f and 10f seconds
+    IEnumerator waitRandom()
+    {
+        float randomSeconds = Random.Range(3.0f, 10.0f);
+        yield return new WaitForSeconds(randomSeconds);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
